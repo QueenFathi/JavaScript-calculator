d1a2a7074c49e26c0c7464d9bc2f4d252da45c6b
QueenFathi <105749763+QueenFathi@users.noreply.github.com>
2022-06-25 12:54:59 +0100

    file commit

---
+++ scriptcalc.js
@@ -0,0 +1,35 @@
+function ValidatefirstInput(firstInput) {
+    if (isNaN(firstInput)) {
+        alert("Refresh and enter a valid number");
+    } else {
+        return firstInput;
+    }
+}
+var firstInput = ValidatefirstInput(parseFloat(prompt("Enter a number")));
+
+var operator = prompt("Specify the operation: /,  +, -, * ");
+
+function ValidateSecondInput(secInput) {
+    if (isNaN(secInput)) {
+        alert("Refresh and enter a valid second number");
+    } else {
+        return secInput;
+    }
+}
+var secInput = ValidateSecondInput(parseFloat(prompt("Enter the second number")))
+
+let result = 0
+
+if (operator=="+"){
+    result = firstInput + secInput;
+} else if (operator=="-"){
+    result= firstInput - secInput;
+} else if (operator=="/"){
+    result = firstInput / secInput;
+} else if (operator=="*"){
+    result = firstInput * secInput;
+} else {
+    alert("Refresh and enter any operator of the following: /,  +, -, * ")
+}
+
+alert(firstInput+operator+secInput+"= "+result);
